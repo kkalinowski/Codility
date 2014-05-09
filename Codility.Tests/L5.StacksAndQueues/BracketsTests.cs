2a09a9fae852ab9245c3f9d15b9196402e82f529
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-05-09 13:16:29 +0200

    Done L5.Brackets

---
+++ Codility.Tests/L5.StacksAndQueues/BracketsTests.cs
@@ -0,0 +1,33 @@
+﻿using Codility.L5.StacksAndQueues;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests.L5.StacksAndQueues
+{
+    public class BracketsTests
+    {
+        [Fact]
+        public void valid()
+        {
+            var S = "{[()()]}";
+
+            Brackets.solution(S).Should().Be(1);
+        }
+
+        [Fact]
+        public void invalid()
+        {
+            var S = "([)()]";
+
+            Brackets.solution(S).Should().Be(0);
+        }
+
+        [Fact]
+        public void invalid2()
+        {
+            var S = "((())()";
+
+            Brackets.solution(S).Should().Be(0);
+        }
+    }
+}
---
+++ Codility/L5.StacksAndQueues/Brackets.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Codility.L5.StacksAndQueues
+{
+    public static class Brackets
+    {
+        public static int solution(string S)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var sign in S)
+            {
+                if (sign == '(' || sign == '{' || sign == '[')
+                {
+                    stack.Push(sign);
+                    continue;
+                }
+
+                if ((sign == ')' || sign == '}' || sign == ']') && stack.Count == 0)
+                    return 0;
+
+                if (sign == ')')
+                {
+                    var prevSign = stack.Pop();
+                    if (prevSign != '(')
+                        return 0;
+                }
+                if (sign == '}')
+                {
+                    var prevSign = stack.Pop();
+                    if (prevSign != '{')
+                        return 0;
+                }
+                if (sign == ']')
+                {
+                    var prevSign = stack.Pop();
+                    if (prevSign != '[')
+                        return 0;
+                }
+            }
+
+            return stack.Count == 0 ? 1 : 0;
+        }
+    }
+}
