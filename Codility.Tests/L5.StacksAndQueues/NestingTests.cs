fc862a99f8007bd3b4ee98061f7a004eea1fdc8d
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-05-09 13:24:36 +0200

    Done L6.Nesting

---
+++ Codility.Tests/L5.StacksAndQueues/NestingTests.cs
@@ -0,0 +1,25 @@
+﻿using Codility.L5.StacksAndQueues;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests.L5.StacksAndQueues
+{
+    public class NestingTests
+    {
+        [Fact]
+        public void valid()
+        {
+            var S = "(()(())())";
+
+            Nesting.solution(S).Should().Be(1);
+        }
+
+        [Fact]
+        public void invalid()
+        {
+            var S = "())";
+
+            Nesting.solution(S).Should().Be(0);
+        }
+    }
+}
---
+++ Codility/L5.StacksAndQueues/Nesting.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Codility.L5.StacksAndQueues
+{
+    public static class Nesting
+    {
+        public static int solution(string S)
+        {
+            var opened = 0;
+
+            foreach (var sign in S)
+            {
+                if (sign == '(')
+                {
+                    opened++;
+                }
+                else if (sign == ')')
+                {
+                    opened--;
+                    if (opened < 0)
+                        return 0;
+                }
+            }
+
+            return opened == 0 ? 1 : 0;
+        }
+    }
+}
