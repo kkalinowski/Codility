2ca18ebce7fc9ddbde845d005188aaa75c104c95
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-05-10 00:54:09 +0200

    Done L5.StoneWall

---
+++ Codility.Tests/L5.StacksAndQueues/StoneWallTests.cs
@@ -0,0 +1,17 @@
+﻿using Codility.L5.StacksAndQueues;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests.L5.StacksAndQueues
+{
+    public class StoneWallTests
+    {
+        [Fact]
+        public void example()
+        {
+            var H = new[] { 8, 8, 5, 7, 9, 8, 7, 4, 8 };
+
+            StoneWall.solution(H).Should().Be(7);
+        }
+    }
+}
---
+++ Codility/L5.StacksAndQueues/StoneWall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Codility.L5.StacksAndQueues
+{
+    public static class StoneWall
+    {
+        public static int solution(int[] H)
+        {
+            var N = H.Length;
+            var currentHeight = H[0];
+            var foundamentBlocks = new Stack<int>();
+            var usedBlocks = 1;
+            foundamentBlocks.Push(currentHeight);
+
+            for (var i = 1; i < N; i++)
+            {
+                while (foundamentBlocks.Count > 0 && H[i] < currentHeight)
+                {
+                    var lastHeight = foundamentBlocks.Pop();
+                    currentHeight -= lastHeight;
+                }
+
+                if (H[i] > currentHeight)
+                {
+                    foundamentBlocks.Push(H[i] - currentHeight);
+                    currentHeight = H[i];
+                    usedBlocks++;
+                }
+            }
+
+            return usedBlocks;
+        }
+    }
+}
