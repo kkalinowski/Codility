df76d9857da1d471d7c97fc86e133e4c1e87f387
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-05-13 13:43:33 +0200

    L6.Dominator done

---
+++ Codility.Tests/L6.Leader/DominatorTests.cs
@@ -0,0 +1,17 @@
+﻿using Codility.L6.Leader;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests.L6.Leader
+{
+    public class DominatorTests
+    {
+        [Fact]
+        public void example()
+        {
+            var A = new[] { 3, 4, 3, 2, 3, -1, 3, 3 };
+
+            Dominator.solution(A).Should().BeOneOf(new[] {0, 2, 4, 6, 7});
+        }
+    }
+}
---
+++ Codility/L6.Leader/Dominator.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+namespace Codility.L6.Leader
+{
+    public static class Dominator
+    {
+        public static int solution(int[] A)
+        {
+            var N = A.Length;
+            var stackValue = 0;
+            var stackSize = 0;
+            var candidateIndex = -1;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (stackSize == 0)
+                {
+                    stackValue = A[i];
+                    stackSize++;
+                    candidateIndex = i;
+                }
+                else
+                {
+                    if (stackValue != A[i])
+                        stackSize--;
+                    else
+                        stackSize++;
+                }
+            }
+
+            if (stackSize == 0)
+                return -1;
+
+            return A.Count(x => x == stackValue) > N / 2.0 ? candidateIndex : -1;
+        }
+    }
+}
