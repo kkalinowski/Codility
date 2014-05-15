b32621e7f97ff83b090948e5d80bc717a8b4c29c
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-05-15 02:31:56 +0200

    L6.EquiLeader done

---
+++ Codility.Tests/L6.Leader/EquiLeaderTests.cs
@@ -0,0 +1,17 @@
+﻿using Codility.L6.Leader;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests.L6.Leader
+{
+    public class EquiLeaderTests
+    {
+        [Fact]
+        public void example()
+        {
+            var A = new[] { 4, 3, 4, 4, 4, 2 };
+
+            EquiLeader.solution(A).Should().Be(2);
+        }
+    }
+}
---
+++ Codility/L6.Leader/EquiLeader.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+
+namespace Codility.L6.Leader
+{
+    public static class EquiLeader
+    {
+        public static int solution(int[] A)
+        {
+            var N = A.Length;
+            var leader = FindCandidateForLeader(A);
+            var leaderCount = A.Count(x => x == leader);
+
+            if (leaderCount <= N / 2.0)
+                return 0;
+
+            var currentLeaderCount = 0;
+            var equiLeadersCount = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (A[i] == leader)
+                    currentLeaderCount++;
+
+                if (currentLeaderCount > (i + 1) / 2.0 && (leaderCount - currentLeaderCount) > (N - i - 1) / 2.0)
+                    equiLeadersCount++;
+            }
+
+            return equiLeadersCount;
+        }
+
+        private static int FindCandidateForLeader(int[] A)
+        {
+            var N = A.Length;
+            var stackValue = 0;
+            var stackSize = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (stackSize == 0)
+                {
+                    stackValue = A[i];
+                    stackSize++;
+                }
+                else
+                {
+                    if (stackValue != A[i])
+                        stackSize--;
+                    else
+                        stackSize++;
+                }
+            }
+
+            return stackValue;
+        }
+    }
+}
