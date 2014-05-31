ad1597d27a5088db1762d97ba0cfdce362e68fd1
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-05-31 20:12:24 +0200

    L7.MaxDoubleSlice

---
+++ Codility.Tests/L7.MaximumSliceProblem/MaxDoubleSliceSumTests.cs
@@ -0,0 +1,17 @@
+﻿using Codility.L7.MaximumSliceProblem;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests.L7.MaximumSliceProblem
+{
+    public class MaxDoubleSliceSumTests
+    {
+        [Fact]
+        public void example()
+        {
+            var A = new[] { 3, 2, 6, -1, 4, 5, -1, 2 };
+
+            MaxDoubleSliceSum.solution(A).Should().Be(17);
+        }
+    }
+}
---
+++ Codility/L7.MaximumSliceProblem/MaxDoubleSliceSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Codility.L7.MaximumSliceProblem
+{
+    public static class MaxDoubleSliceSum
+    {
+        public static int solution(int[] A)
+        {
+            var N = A.Length;
+            var K1 = new int[N];
+            var K2 = new int[N];
+
+            for (int i = 1; i < N - 1; i++)
+                K1[i] = Math.Max(K1[i - 1] + A[i], 0);
+
+            for (int i = N - 2; i > 0; i--)
+                K2[i] = Math.Max(K2[i + 1] + A[i], 0);
+
+            var max = 0;
+            for (int i = 1; i < N - 1; i++)
+                max = Math.Max(max, K1[i - 1] + K2[i + 1]);
+
+            return max;
+        }
+    }
+}
