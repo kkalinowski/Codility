828bbced85151a67cfde69d3a136bad85962c6d0
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-06-17 23:26:42 +0200

    L7.MaxProfit done

---
+++ Codility.Tests/L7.MaximumSliceProblem/MaxProfitTests.cs
@@ -0,0 +1,17 @@
+﻿using Codility.L7.MaximumSliceProblem;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests.L7.MaximumSliceProblem
+{
+    public class MaxProfitTests
+    {
+        [Fact]
+        public void example()
+        {
+            var A = new[] { 23171, 21011, 21123, 21366, 21013, 21367 };
+
+            MaxProfit.solution(A).Should().Be(356);
+        }
+    }
+}
---
+++ Codility/L7.MaximumSliceProblem/MaxProfit.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Codility.L7.MaximumSliceProblem
+{
+    public static class MaxProfit
+    {
+        public static int solution(int[] A)
+        {
+            var N = A.Length;
+            if (N < 1)
+                return 0;
+
+            var max = 0;
+            var result = 0;
+
+            for (int i = N - 1; i >= 0; --i)
+            {
+                if (A[i] > max)
+                    max = A[i];
+
+                var temp = max - A[i];
+                if (temp > result)
+                    result = temp;
+            }
+
+            return result;
+        }
+    }
+}
