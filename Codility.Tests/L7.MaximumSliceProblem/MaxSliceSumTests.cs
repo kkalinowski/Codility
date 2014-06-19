8eb0c67261064e6850463daf724e80e32a254441
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-06-19 13:12:16 +0200

    L7.MaxSliceSum done

---
+++ Codility.Tests/L7.MaximumSliceProblem/MaxSliceSumTests.cs
@@ -0,0 +1,25 @@
+﻿using Codility.L7.MaximumSliceProblem;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests.L7.MaximumSliceProblem
+{
+    public class MaxSliceSumTests
+    {
+        [Fact]
+        public void example()
+        {
+            var A = new[] { 3, 2, -6, 4, 0 };
+
+            MaxSliceSum.solution(A).Should().Be(5);
+        }
+
+        [Fact]
+        public void one_element()
+        {
+            var A = new[] { -10 };
+
+            MaxSliceSum.solution(A).Should().Be(-10);
+        }
+    }
+}
