e1d7bc2735cb6091cec99feccb7041860bcce494
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-06-19 13:49:43 +0200

    L8.MinPerimeterRectangle

---
+++ Codility.Tests/L8.PrimeAndCompositeNumbers/MinPerimeterRectangleTests.cs
@@ -0,0 +1,15 @@
+﻿using Codility.L8.PrimeAndCompositeNumbers;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests.L8.PrimeAndCompositeNumbers
+{
+    public class MinPerimeterRectangleTests
+    {
+        [Fact]
+        public void example()
+        {
+            MinPerimeterRectangle.solution(30).Should().Be(22);
+        }
+    }
+}
