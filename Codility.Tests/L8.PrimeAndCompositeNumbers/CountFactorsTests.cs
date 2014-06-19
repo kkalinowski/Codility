02402ffe966ad1799252b5f9888880da77cad50d
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-06-19 14:11:00 +0200

    L8.CountFactors

---
+++ Codility.Tests/L8.PrimeAndCompositeNumbers/CountFactorsTests.cs
@@ -0,0 +1,15 @@
+﻿using Codility.L8.PrimeAndCompositeNumbers;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests.L8.PrimeAndCompositeNumbers
+{
+    public class CountFactorsTests
+    {
+        [Fact]
+        public void example()
+        {
+            CountFactors.solution(24).Should().Be(8);
+        }
+    }
+}
